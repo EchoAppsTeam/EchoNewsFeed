1ff169134f4b8801cdc2dd06245581501721ffc2
Ilya Mikhailov <mikhailov@aboutecho.com>
2014-02-10 15:35:46 +0400

    app config cleanup is done; config normalizer is removed;

--- app.js
+++ app.js
@@ -8,6 +8,40 @@ if (Echo.App.isDefined("Echo.Apps.NewsFeed")) return;
 var newsFeed = Echo.App.manifest("Echo.Apps.NewsFeed");
 
 newsFeed.config = {
+	"tabs": {
+		"official": {
+			"enabled": true,
+			"label": "Official"
+		}
+	},
+	"content": {
+		"section": "local",
+		"tags": "",
+		"officialSources":""
+	},
+	"presentation": {
+		"displayUsernames": false,
+		"displayAvatars": false
+	},
+	"replies": {
+		"displayTweets": false,
+		"displayNativeReplies": "all"
+	},
+	"dependencies": {
+		"Janrain": {
+			"appId": undefined
+		},
+		"StreamServer": {
+			"appkey": undefined
+		}
+	},
+	"postComposerActivatingTabs": [
+		"all"
+	],
+	"advanced": {}
+};
+
+newsFeed.vars = {
 	"contentTabs": [{
 		"id": "official",
 		"active": true,
@@ -27,23 +61,7 @@ newsFeed.config = {
 		"type": "tab",
 		"label": "All Contributors"
 	}],
-	"presentation": {
-		"displayUsernames": false,
-		"displayAvatars": false
-	},
-	"dependencies": {
-		"Janrain": {
-			"appId": undefined
-		},
-		"StreamServer": {
-			"appkey": undefined
-		}
-	},
-	"queries": {},
-	"postComposerActivatingTabs": [
-		"all"
-	],
-	"advanced": {}
+	"queries": undefined
 };
 
 newsFeed.dependencies = [{
@@ -72,82 +90,10 @@ newsFeed.templates.tabs.navItem =
 		'<a href="#{data:tabId}" id="{data:tabId}" data-toggle="{data:type}" >{data:label}</a>' +
 	'</li>';
 
-newsFeed.config.normalizer = {
-	"contentTabs": function(obj, component) {
-		var tabs = component.config.tabs;
-		var result = [];
-		$.map(obj, function(tab) {
-			if (tabs && tabs[tab.id]) {
-				if (tabs[tab.id] && tabs[tab.id].enabled === false) {
-					tab.visible = false;
-				}
-				if (tabs[tab.id] && tabs[tab.id].label) {
-					tab.label = tabs[tab.id].label;
-				}
-			}
-			result.push(tab);
-		});
-		return result;
-	},
-	"queries": function(obj, component) {
-		if (!component.config.content) {
-			component.config.content = {};
-		}
-		var tags = component.config.content.tags;
-		var sources = component.config.content.officialSources;
-		var section = component.config.content.section;
-		var commonQueryParts = [];
-		if (section && section.toLowerCase() !== "local") {
-			commonQueryParts.push("childrenof:" + section);
-		} else {
-			var pageURL = $("link[rel='canonical']").attr('href');
-			if(!pageURL || !Echo.Utils.parseURL(pageURL).domain) {
-				pageURL =  document.location.href.split("#")[0];
-			}
-			commonQueryParts.push("childrenof:" + pageURL);
-		}
-		if (sources && typeof sources === "string" && sources.length > 0) {
-			commonQueryParts.push("source:" + sources.replace(" ", ""));
-		}
-		if (tags && typeof tags === "string" && tags.length > 0) {
-			commonQueryParts.push("tags:" + tags.toLowerCase().replace(" ", ""));
-		}
-		var displayTweets =  component.get("config.replies.displayTweets") || false;
-		var nativeSubmissionsMode = component.get("config.replies.displayNativeReplies") || "all";
-		commonQueryParts.push("-state:ModeratorDeleted ");
-		var childrenQueryPart = "children:2 -state:ModeratorDeleted";
-		if (nativeSubmissionsMode === "none") {
-			if (!displayTweets) {
-				childrenQueryPart = "children:0";
-			} else {
-				childrenQueryPart += " source:Twitter";
-			}
-		} else if (nativeSubmissionsMode === "top") {
-			if (!displayTweets) {
-				childrenQueryPart += " -source:Twitter";
-			} else {
-				childrenQueryPart += " source:Twitter";
-			}
-			childrenQueryPart += " OR (user.markers:Conversations.TopContributor OR markers:Conversations.TopPost)";
-		} else if (nativeSubmissionsMode === "all") {
-			if (!displayTweets) {
-				childrenQueryPart += " -source:Twitter";
-			}
-		}
-		commonQueryParts.push(childrenQueryPart);
-		commonQueryParts.push("safeHTML:permissive");
-		return {
-			"official": commonQueryParts.join(" "),
-			"top": commonQueryParts.join(" "),
-			"all": commonQueryParts.join(" ")
-		};
-	}
-};
-
 newsFeed.renderers.postsTabs = function(element, extra) {
 	var self = this;
 	var tpls = this.templates.tabs;
-	var tabs = this.config.get("contentTabs");
+	var tabs = this._initContentTabs();
 	var nav = $(this.substitute({"template": tpls.nav}));
 	var visibleTabsCounter = 0;
 	$.map(tabs, function(tab) {
@@ -171,7 +117,6 @@ newsFeed.renderers.postsTabs = function(element, extra) {
 				} else {
 					conversationsStream.config.set("postComposer.visible", false);
 				}
-				
 				conversationsStream.refresh();
 			});
 			nav.append(container);
@@ -181,15 +126,6 @@ newsFeed.renderers.postsTabs = function(element, extra) {
 		return element.empty();
 	}
 	return element.empty().append(nav);
-};
-
-newsFeed.methods._getQueryByActiveTabId = function(activeTabId) {
-	var queries = this.config.get("queries");
-	if (queries[activeTabId]) {
-		return queries[activeTabId];
-	} else {
-		return queries.official;
-	}
 };
 
 newsFeed.renderers.newsFeed = function(element) {
@@ -222,6 +158,95 @@ newsFeed.renderers.newsFeed = function(element) {
 		}, this.config.get("advanced"))
 	});
 	return element;
+};
+
+newsFeed.methods._initContentTabs = function() {
+	var tabs = this.config.get("tabs");
+	var result = [];
+	$.map(this.get("contentTabs"), function(tab) {
+		if (tabs && tabs[tab.id]) {
+			if (tabs[tab.id] && tabs[tab.id].enabled === false) {
+				tab.visible = false;
+			}
+			if (tabs[tab.id] && tabs[tab.id].label) {
+				tab.label = tabs[tab.id].label;
+			}
+		}
+		result.push(tab);
+	});
+	return result;
+};
+
+newsFeed.methods._getQueryByActiveTabId = function(activeTabId) {
+	var queries = this.get("queries");
+	if (!queries) {
+		queries = this._formatQueries();
+	}
+	if (queries[activeTabId]) {
+		return queries[activeTabId];
+	} else {
+		return queries.official;
+	}
+};
+
+newsFeed.methods._formatQueries = function() {
+	var contentParams = this.config.get("content");
+	var commonQueryParts = [];
+
+	if (contentParams.section && contentParams.section.toLowerCase() !== "local") {
+		commonQueryParts.push("childrenof:" + contentParams.section);
+	} else {
+		var pageURL = $("link[rel='canonical']").attr('href');
+		if(!pageURL || !Echo.Utils.parseURL(pageURL).domain) {
+			pageURL =  document.location.href.split("#")[0];
+		}
+		commonQueryParts.push("childrenof:" + pageURL);
+	}
+	if (contentParams.officialSources && contentParams.officialSources.length > 0) {
+		var sources = $.map(contentParams.officialSources.replace(" ", "").split(","), function(item) {
+			return Echo.Utils.capitalize(item);
+		});
+		commonQueryParts.push("source:" + sources.join(","));
+	}
+	if (contentParams.tags && contentParams.tags.length > 0) {
+		commonQueryParts.push("tags:" + contentParams.tags.toLowerCase().replace(" ", ""));
+	}
+
+	commonQueryParts.push("-state:ModeratorDeleted");
+	var displayTweets = this.config.get("replies.displayTweets") || false;
+	var nativeSubmissionsMode = this.config.get("replies.displayNativeReplies") || "all";
+
+	commonQueryParts.push(this._formatChildrenQueryPart(displayTweets, nativeSubmissionsMode));
+	commonQueryParts.push("safeHTML:permissive");
+
+	return {
+		"official": commonQueryParts.join(" "),
+		"top": commonQueryParts.join(" "),
+		"all": commonQueryParts.join(" ")
+	};
+};
+
+newsFeed.methods._formatChildrenQueryPart = function(displayTweets, nativeSubmissionsMode) {
+	var childrenQueryPart = "children:2 -state:ModeratorDeleted";
+	if (nativeSubmissionsMode === "none") {
+		if (!displayTweets) {
+			childrenQueryPart = "children:0";
+		} else {
+			childrenQueryPart += " source:Twitter";
+		}
+	} else if (nativeSubmissionsMode === "top") {
+		if (!displayTweets) {
+			childrenQueryPart += " -source:Twitter";
+		} else {
+			childrenQueryPart += " source:Twitter";
+		}
+		childrenQueryPart += " OR (user.markers:Conversations.TopContributor OR markers:Conversations.TopPost)";
+	} else if (nativeSubmissionsMode === "all") {
+		if (!displayTweets) {
+			childrenQueryPart += " -source:Twitter";
+		}
+	}
+	return childrenQueryPart;
 };
 
 newsFeed.css =
